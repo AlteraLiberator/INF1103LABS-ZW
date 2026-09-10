4a1e9f8e3e3128defc9ea5665d3167a83aa81915
Zi Wen <2603063@sit.singaporetech.edu.sg>
2026-09-11 00:33:49 +0800

    Req 2

--- Lab2/auditor.py
+++ Lab2/auditor.py
@@ -1,1 +1,9 @@
 inventory = 0
+
+# Main Program Loop
+while True:
+    mainMenu = input("Enter stock quantity, or 'quit' to exit: ").lower()
+
+    # Exit the program if the user types 'quit'
+    if mainMenu == "quit":
+        break
