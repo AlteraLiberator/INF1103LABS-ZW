8d41c17af68d373f41b44ba827dcf8da7f58411b
Zi Wen <2603063@sit.singaporetech.edu.sg>
2026-09-18 15:22:44 +0800

    Added generate report functionality, working on main function

--- Lab3/modular_auditor.py
+++ Lab3/modular_auditor.py
@@ -3,16 +3,15 @@ INVENTORY_CAPACITY = 500
 TAX_RATE = 0.09     # 9% GST lul
 VALUE_PER_UNIT_ITEM = 120   # Has to have value to be taxed
 
-# Initialising global variables
-inventory = 0
-rejectedEntries = 0
 
 # Functions
 
-# Get user input
+# Get user input (Returns either boolean or tuple)
 def get_valid_input():
 
+    # Initialise variables
     user_input = ""
+    rejected_entries = 0
 
     # Mini while loop to ensure that we do not return invalid input
     while True:
@@ -27,7 +26,7 @@ def get_valid_input():
         # If user entered a non-positive number that isnt 'quit'
         if not user_input.isDigit():
             # Add failed counter to global variable rejectedEntries
-            rejectedEntries += 1
+            rejected_entries += 1
 
             # Note isdigit() will not count -ve numbers input as number as "-1".isdigit() will count the "-" as a non digit character, thus failing the check
             print("Invalid input. Please enter a positive number or 'quit'.")
@@ -36,14 +35,14 @@ def get_valid_input():
         # If user entered 0
         if int(user_input) == 0:
             # Add failed counter to global variable rejectedEntries
-            rejectedEntries += 1
+            rejected_entries += 1
             print("Invalid input. Please enter a positive number or 'quit'.")
             pass
 
         # Return proper(This does break the loop)
-        return int(user_input)
+        return (int(user_input), rejected_entries)
 
-# Adds delivery to current inventory
+# Adds delivery to current inventory (returns new total)
 def process_delivery(current_total, new_value):
     # Initialise variables
     overflow = False
@@ -68,8 +67,37 @@ def calculate_tax(delivery_amount):
 
     return tax
 
-def generate_report():
-    pass
+# Generates the text string for the report (DOES NOT PRINT)
+def generate_report(inventory_units, rejected_entries):
+
+    # Initialise variables
+    report = ""
+    report += "Final Session Report\n"
+    report += "Total units in store: {}.\n".format(inventory_units)
+    report += "Failed entry attempts: {}.\n".format(rejected_entries)
+
+    return report
+
+def main():
+
+    # Initialise variables
+    inventory = 0
+    total_failed_attempts = 0
+
+    # Start of main loop
+    while True:
+        user_input = get_valid_input()
+
+        # get_valid_input returned false (user entered quit)
+        if not user_input:
+            print(generate_report(inventory, total_failed_attempts))
+            break
+
+        delivery_units, rejected_user_inputs = user_input
+        total_failed_attempts += rejected_user_inputs
+
+        process_delivery(inventory, delivery_units)
+
 
 
 # Main Program Loop
